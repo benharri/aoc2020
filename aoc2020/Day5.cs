c5859f8d8ac701d3164b2067fe1981f242cbab9c
Ben Harris <ben@tilde.team>
2020-12-05 12:37:37 -0500

    idk this feels tidier

--- aoc2020/Day5.cs
+++ aoc2020/Day5.cs
@@ -1,40 +1,32 @@
 using System;
+using System.Collections.Immutable;
 using System.Linq;
 
 namespace aoc2020
 {
     public sealed class Day5 : Day
     {
-        private readonly bool[] _ids;
-        private readonly int _max;
+        private readonly ImmutableHashSet<int> _ids;
 
         public Day5()
         {
-            _ids = new bool[1024];
-            _max = 0;
-            foreach (var l in Input
-                .Select(s => s.Replace('F', '0').Replace('B', '1').Replace('L', '0').Replace('R', '1'))
-                .Select(s => Convert.ToInt32(s, 2)))
-            {
-                if (l > _max) _max = l;
-                _ids[l] = true;
-            }
+            _ids = Input
+                .Select(s =>
+                    Convert.ToInt32(s.Replace('F', '0').Replace('B', '1').Replace('L', '0').Replace('R', '1'), 2))
+                .OrderBy(i => i)
+                .ToImmutableHashSet();
         }
 
         public override int DayNumber => 5;
 
         public override string Part1()
         {
-            return $"{_max}";
+            return $"{_ids.Max()}";
         }
 
         public override string Part2()
         {
-            for (var i = 1; i < _ids.Length; ++i)
-                if (_ids[i - 1] && !_ids[i])
-                    return $"{i}";
-
-            return "";
+            return $"{Enumerable.Range(_ids.First(), _ids.Count + 1).Single(i => !_ids.Contains(i))}";
         }
     }
 }
