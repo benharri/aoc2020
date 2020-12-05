9c701b3bdc46871bfb464a305d66ac215da46820
Ben Harris <ben@tilde.team>
2020-12-05 11:56:27 -0500

    it's just a single binary number... splitting and *8 is unnecessary

--- aoc2020/Day5.cs
+++ aoc2020/Day5.cs
@@ -12,7 +12,9 @@ namespace aoc2020
         {
             _ids = new bool[1024];
             _max = 0;
-            foreach (var l in Input.Select(Id))
+            foreach (var l in Input
+                .Select(s => s.Replace('F', '0').Replace('B', '1').Replace('L', '0').Replace('R', '1'))
+                .Select(s => Convert.ToInt32(s, 2)))
             {
                 if (l > _max) _max = l;
                 _ids[l] = true;
@@ -21,13 +23,6 @@ namespace aoc2020
 
         public override int DayNumber => 5;
 
-        private static int Id(string s)
-        {
-            var row = Convert.ToInt32(s[..7].Replace('F', '0').Replace('B', '1'), 2);
-            var col = Convert.ToInt32(s[7..].Replace('L', '0').Replace('R', '1'), 2);
-            return row * 8 + col;
-        }
-
         public override string Part1()
         {
             return $"{_max}";
@@ -35,7 +30,7 @@ namespace aoc2020
 
         public override string Part2()
         {
-            for (var i = 1; i < 1024; ++i)
+            for (var i = 1; i < _ids.Length; ++i)
                 if (_ids[i - 1] && !_ids[i])
                     return $"{i}";
 
