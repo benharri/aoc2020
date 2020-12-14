97b58586dd70864ad6a7c33fe833f06c5064db20
Ben Harris <ben@tilde.team>
2020-12-14 09:59:50 -0500

    start day 14

---
+++ aoc2020/Day14.cs
@@ -0,0 +1,22 @@
+namespace aoc2020
+{
+    /// <summary>
+    ///     Day 14: <see href="https://adventofcode.com/2020/day/14">Docking Data</see>
+    /// </summary>
+    public sealed class Day14 : Day
+    {
+        public Day14() : base(14)
+        {
+        }
+
+        public override string Part1()
+        {
+            return "";
+        }
+
+        public override string Part2()
+        {
+            return "";
+        }
+    }
+}
