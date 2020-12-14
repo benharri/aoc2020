ca1b036ce7751299cef30098c5737c170cc1c112
Ben Harris <ben@tilde.team>
2020-12-14 17:49:42 -0500

    remove unused extension methods

--- aoc2020/Extensions.cs
+++ aoc2020/Extensions.cs
@@ -1,28 +1,9 @@
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
 
 namespace aoc2020
 {
     public static class Extensions
     {
-        public static string TrimEnd(this string source, string value)
-        {
-            return !source.EndsWith(value)
-                ? source
-                : source.Remove(source.LastIndexOf(value, StringComparison.Ordinal));
-        }
-
-        public static IEnumerable<IEnumerable<T>> Combinations<T>(this IEnumerable<T> elements, int k)
-        {
-            var enumerable = elements as T[] ?? elements.ToArray();
-            return k == 0
-                ? new[] {Array.Empty<T>()}
-                : enumerable.SelectMany((e, i) =>
-                    enumerable.Skip(i + 1).Combinations(k - 1).Select(c => new[] {e}.Concat(c)));
-        }
-
         /// <summary>
         ///     increased accuracy for stopwatch based on frequency.
         ///     <see
