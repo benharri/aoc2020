5b0f00f26977b209212c93bc079d3417c3c9e660
Ben Harris <ben@tilde.team>
2021-11-11 00:17:22 -0500

    use new Main format

--- aoc2020/Program.cs
+++ aoc2020/Program.cs
@@ -1,27 +1,18 @@
-﻿using System.Reflection;
+﻿using aoc2020;
+using System.Reflection;
 
-namespace aoc2020;
+var days = Assembly.GetExecutingAssembly().GetTypes()
+    .Where(t => t.BaseType == typeof(Day))
+    .Select(t => (Activator.CreateInstance(t) as Day)!)
+    .OrderBy(d => d.DayNumber);
 
-internal static class Program
+if (args.Length == 1 && int.TryParse(args[0], out var dayNum))
 {
-    private static void Main(string[] args)
-    {
-        var days = Assembly.GetExecutingAssembly().GetTypes()
-            .Where(t => t.BaseType == typeof(Day))
-            .Select(t => Activator.CreateInstance(t) as Day)
-            .OrderBy(d => d?.DayNumber);
-
-        if (args.Length == 1 && int.TryParse(args[0], out var dayNum))
-        {
-            var day = days.FirstOrDefault(d => d?.DayNumber == dayNum);
-            if (day != null)
-                day.AllParts();
-            else
-                Console.WriteLine($"Day {dayNum} invalid or not yet implemented");
-        }
-        else
-        {
-            foreach (var d in days) d?.AllParts();
-        }
-    }
+    var day = days.FirstOrDefault(d => d.DayNumber == dayNum);
+    if (day != null) day.AllParts();
+    else Console.WriteLine($"Day {dayNum} invalid or not yet implemented");
+}
+else
+{
+    foreach (var d in days) d.AllParts();
 }
